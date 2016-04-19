b772f163aec003a17abc451e4c78368fd36b31b5
Kistler <kistler@marek.sedlacek@kistler.com>
2016-04-19 18:49:20 +0200

    initial commit

---
+++ src/clojure_noob/core.clj
@@ -0,0 +1,86 @@
+(ns clojure-noob.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, I am a teapot!"))
+
+(def asym-hobbit-body-parts [{:name "head" :size 3}
+                             {:name "left-eye" :size 1}
+                             {:name "left-ear" :size 1}
+                             {:name "mouth" :size 1}
+                             {:name "nose" :size 1}
+                             {:name "neck" :size 2}
+                             {:name "left-shoulder" :size 3}
+                             {:name "left-upper-arm" :size 3}
+                             {:name "chest" :size 10}
+                             {:name "back" :size 10}
+                             {:name "left-forearm" :size 3}
+                             {:name "abdomen" :size 6}
+                             {:name "left-kidney" :size 1}
+                             {:name "left-hand" :size 2}
+                             {:name "left-knee" :size 2}
+                             {:name "left-thigh" :size 4}
+                             {:name "left-lower-leg" :size 3}
+                             {:name "left-achilles" :size 1}
+                             {:name "left-foot" :size 2}])
+
+(defn matching-part
+  [part]
+  {:name (clojure.string/replace (:name part) #"^left-" "right-")
+   :size (:size part)})
+
+(defn symmetrize-body-parts
+       [asym-body-parts]
+  (reduce (fn [final-body-parts part]
+            (into final-body-parts (set [part (matching-part part)])))
+          []
+          asym-body-parts))
+
+(defn hit
+  [asym-body-parts]
+  (let [sym-parts (symmetrize-body-parts asym-body-parts)
+        body-part-size-sum (reduce + (map :size sym-parts))
+        target (rand body-part-size-sum)]
+    (loop [[part & remaining] sym-parts
+           accumulated-size (:size part)]
+      (if (> accumulated-size target)
+        part
+        (recur remaining (+ accumulated-size (:size (first remaining))))))))
+
+(defn add100
+  [n]
+  (+ 100 n))
+
+(defn dec-maker
+  [dec-by]
+  #(- % dec-by))
+
+(defn mapset
+  [f a]
+  (apply sorted-set (map f a)))
+
+(mapset inc [1 1 2 2])
+
+(def alien-head {:name "head" :size 2})
+(def alien-eye {:name "eye-1" :size 1})
+
+(defn make-alien-part
+  [name size i]
+  {:name (str name (inc i)) :size size})
+
+(defn matching-part-n
+  [part n]
+  (if-let [[s] (re-seq #"(.+-)(\d*)" (:name part))]
+    (take n (map (partial make-alien-part (second s) (:size part)) (range)))
+    part))
+
+(matching-part-n alien-eye 5)
+
+(defn alien-symmetrize
+  [asym-body-parts]
+  (reduce (fn [final-body-parts part]
+            (into final-body-parts (set [part (matching-part part)])))
+          []
+          asym-body-parts))
